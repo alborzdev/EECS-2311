7b20fc619c9c6e457f574d01b61244c03c9b73e0
Kamal Patel <kamalpatel291@gmail.com>
2020-01-22 13:23:05 -0500

    Added a "ADD" button.

--- Venn/src/main/java/venn/Main.java
+++ Venn/src/main/java/venn/Main.java
@@ -1,6 +1,8 @@
 package venn;
 
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
@@ -11,32 +13,39 @@ import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class Main extends JPanel implements MouseListener, MouseMotionListener{
+public class Main extends JPanel implements MouseListener{
 	public final static int WIDTH = 500, HEIGHT = 500;
-	private boolean triggered, pressedCancel;
+	private boolean triggered, pressedCancel, addClicked;
 	private JFrame frame;
 	private int xPos, yPos;
 	private String strInput;
 	private int numClicks;
+	private JButton btnAdd;
 	
 	public Main() {
 		frame = new JFrame("Customizable Venn Diagram");
 		frame.setSize(WIDTH, HEIGHT);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setLocationRelativeTo(null);
+		frame.setLocationRelativeTo(null);// set the location of the frame to be in the middle.
 		frame.setResizable(false);
 		frame.setLayout(null);
 		frame.add(this);
 		frame.setVisible(true);
 		this.setSize(WIDTH,HEIGHT);
-		this.addMouseListener(this);
-		this.addMouseMotionListener(this);
+		frame.addMouseListener(this);
+		//this.addMouseMotionListener(this);
 		triggered = false;
 		pressedCancel = false;
 		xPos = WIDTH/2;
 		yPos = HEIGHT/2;
 		strInput = "Test";
 		numClicks = 0;
+		
+		btnAdd = new JButton("ADD");
+		btnAdd.setBounds((WIDTH/2)-50, HEIGHT-100, 100, 50);
+		btnAdd.addMouseListener(this);
+		frame.add(btnAdd);
+		
 	//	circles = new ArrayList<Circle>();
 		
 //		circles.add(new Circle(WIDTH - (WIDTH/4), HEIGHT/2, 100, 100));
@@ -53,7 +62,10 @@ public class Main extends JPanel implements MouseListener, MouseMotionListener{
 			g.drawOval((WIDTH/4)-50, HEIGHT/2-100, 200, 200);
 			g.drawOval((WIDTH/4)+70, HEIGHT/2-100, 200, 200);
 		}
-		
+		if(addClicked) {
+			g.drawOval((WIDTH/4)-50, HEIGHT/2-100, 200, 200-numClicks);
+			addClicked = false;
+		}
 		if(triggered) {
 			g.drawString(strInput, xPos, yPos);
 			triggered = false;
@@ -64,23 +76,28 @@ public class Main extends JPanel implements MouseListener, MouseMotionListener{
 	@Override
 	public void mouseClicked(MouseEvent arg0) {
 		// TODO Auto-generated method stub
-		numClicks++;
-		xPos = arg0.getX();
-		yPos = arg0.getY();
-		try {
-			do {
-				strInput = JOptionPane.showInputDialog("Enter Text: ", "0");
-			}while(strInput.equals(null) || strInput.equals("Test"));
-		}catch(Exception e) {
-			strInput = "Test";
-			pressedCancel = true; //when they press cancel on JOptionPane
-		}
-		if(!pressedCancel) {
-			triggered = true;
+		if(arg0.getSource() != btnAdd) {
+			numClicks++;
+			xPos = arg0.getX();
+			yPos = arg0.getY();
+			try {
+				do {
+					strInput = JOptionPane.showInputDialog("Enter Text: ", "0");
+				}while(strInput.equals(null) || strInput.equals("Test"));
+			}catch(Exception e) {
+				strInput = "Test";
+				pressedCancel = true; //when they press cancel on JOptionPane
+			}
+			if(!pressedCancel) {
+				triggered = true;
+				paintComponent(this.getGraphics());
+			}
+			pressedCancel = false;
+		}else if(arg0.getSource() == btnAdd) {
+			numClicks ++;
+			addClicked = true;
 			paintComponent(this.getGraphics());
 		}
-		pressedCancel = false;
-
 	}
 
 	@Override
@@ -98,6 +115,9 @@ public class Main extends JPanel implements MouseListener, MouseMotionListener{
 	@Override
 	public void mousePressed(MouseEvent arg0) {
 		// TODO Auto-generated method stub
+		if(arg0.getSource() == btnAdd) {
+			System.out.print("HELLO");
+		}
 		
 	}
 
@@ -107,17 +127,18 @@ public class Main extends JPanel implements MouseListener, MouseMotionListener{
 		
 	}
 
-	@Override
-	public void mouseDragged(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
+//	@Override
+//	public void mouseDragged(MouseEvent arg0) {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//
+//	@Override
+//	public void mouseMoved(MouseEvent arg0) {
+//		// TODO Auto-generated method stub
+//		
+//	}
 
-	@Override
-	public void mouseMoved(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
+	
 
 }
